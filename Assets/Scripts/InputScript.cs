f149da73f9be71904b260f859d836d2509a95dce
Ar1ad <sora4gamers@gmail.com>
2020-11-17 20:50:34 +0100

    Basic Node System
    Added
    -InputNode
    -AndNode
    (mostly just the visual stuff)
    still gotta work on the scripts

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class InputScript : MonoBehaviour
 {
+
+    Collider2D clickArea;
+    bool output = false;
+    SpriteRenderer circleSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        clickArea =GetComponent<CircleCollider2D>();
+        circleSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (clickArea.OverlapPoint(mousePos))
+            {
+                Debug.Log("Clicked");
+                if (!output)
+                {
+                    output = true;
+                    circleSprite.color = Color.red;
+                }
+                else
+                {
+                    output = false;
+                    circleSprite.color = Color.black;
+                }
+                
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (clickArea.OverlapPoint(mousePos))
+            {
+                Debug.Log("Holding");
+                transform.position = mousePos;
+
+            }
+        }
     }
 }
---
+++ Assets/Scripts/NodeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour
+{
+    Collider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            if (col.OverlapPoint(mousePos))
+            {
+                Debug.Log("Holding");
+                transform.position = mousePos;
+
+            }
+        }
+    }
+}
