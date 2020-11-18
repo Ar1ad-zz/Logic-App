ed9ef2d3b0e4481ee63089189ffa5c883adb1cee
Ar1ad <sora4gamers@gmail.com>
2020-11-18 21:26:10 +0100

    Connection Work #2
    Added
    -Visual Wire
    -Dynamic updater for Switch signal
    -Spawning/Dragging Wire
    
    and smol fixes here and there

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -6,20 +6,26 @@ public class InputScript : MonoBehaviour
 {
 
     Collider2D clickArea;
-    Collider2D wireArea;
+    public Collider2D wireArea;
     bool outputsignal = false;
+    bool isHolding = false;
     SpriteRenderer circleSprite;
-    public Object wireObject;
     public GameObject output;
 
-    //other node
+    // Wire Variables
+    bool wireSpawned = false;
+    public Object wireObject;
+    GameObject wireGameObject;
+
+    // Other Node Stuff
     S_nodeInput hitScript;
+    RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
     {
         clickArea = GetComponent<CircleCollider2D>();
-        wireArea = GetComponentInChildren<CircleCollider2D>();
+        wireArea = transform.Find("InputWireConnector").GetComponent<CircleCollider2D>();
         circleSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
@@ -48,31 +54,53 @@ public class InputScript : MonoBehaviour
             
         }
 
-        // select wireconnector and instantiate wire
-        //if (wireArea.OverlapPoint(mousePos))
-        //{
-        //    if (Input.GetMouseButton(0))
-        //    {
-        //        Instantiate(wireObject);
-        //    }
-
-        //}
-
-        RaycastHit2D hit = new RaycastHit2D();
+        
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics2D.Raycast(ray.origin, ray.direction))
+
+        // select wireconnector and instantiate wire
+        if (Input.GetMouseButton(0))
         {
-            hit = Physics2D.Raycast(ray.origin, ray.direction);
-            if (!Input.GetMouseButtonDown(0))
+            if (wireArea.OverlapPoint(mousePos))
             {
-                hitScript = hit.transform.GetComponent<S_nodeInput>();
-                
-                Collider2D col = hit.transform.GetComponent<CircleCollider2D>();
-                Debug.Log(col + ",  " + col.name);
+                isHolding = true;
+                //Debug.Log("Holding");
             }
-            
+            if (isHolding)
+            {
+                
+                if (!wireSpawned)
+                {
+                    wireSpawned = true;
+                    wireGameObject = (GameObject)Instantiate(wireObject);
+                    
+                    
+                }
+                
+                wireGameObject.GetComponent<LineRenderer>().SetPosition(0, wireArea.transform.position);
+                if (Physics2D.Raycast(ray.origin, ray.direction))
+                {
+                    hit = Physics2D.Raycast(ray.origin, ray.direction);
+                    if (!Input.GetMouseButtonDown(0))
+                    {
+                        hitScript = hit.transform.GetComponent<S_nodeInput>();
+
+                        Collider2D col = hit.transform.GetComponent<CircleCollider2D>();
+
+                        Debug.Log(col + ",  " + col.name);
+
+                        
+                    }
+                }
+
+
+            }
+        }
+        else
+        {
+            isHolding = false;
         }
 
+        wireGameObject.GetComponent<LineRenderer>().SetPosition(1, hit.transform.position);
         hitScript.signal = outputsignal;
         Debug.Log("output: " + outputsignal);
 
