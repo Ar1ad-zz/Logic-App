931ce1b5147ffe5fc24b5e950ec6b20dc3f6e3eb
Ar1ad <sora4gamers@gmail.com>
2020-12-01 17:42:30 +0100

    UI Work
    Started work on the ui

---
+++ Assets/Scripts/UI/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public GameObject nodePanel;
+    
+    public void OpenNodePanel()
+    {
+        Animator anim = nodePanel.GetComponent<Animator>();
+        if(anim != null)
+        {
+            bool isOpen = anim.GetBool("Open");
+            anim.SetBool("Open", !isOpen);
+        }
+    }
+}
