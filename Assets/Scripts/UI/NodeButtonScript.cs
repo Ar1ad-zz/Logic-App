65eadbfeddd3e9afbfb5b097fb03f941d25c6db1
Ar1ad <sora4gamers@gmail.com>
2020-12-07 09:41:30 +0100

    ui stuff

---
+++ Assets/Scripts/UI/NodeButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeButtonScript : MonoBehaviour
+{
+
+    public GameObject node;
+    public void SpawnNode()
+    {
+        for(int i = 0; i < 1; i++){
+            Instantiate(node, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
